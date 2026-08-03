27e861ddb403489582bc73dc7458e80ed3a5a559
Jero Pitkänen <terajero@gmail.com>
2026-08-03 20:03:44 +0300

    unicafe osio tehty 1.6-1.11

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,5 +1,58 @@
 import { useState } from 'react'
 
+// Button komponentti yksittäisen napin näyttämiseen
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+// StatisticLine komponentti yksittäisten tilastotietojen näyttämiseen
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+// Kaikkien tilastotietojen näyttämiseen tarkoitettu komponentti
+const Statistics = ({ good, neutral, bad }) => {
+  // Lasketaan tilastotiedot
+  const all = good + neutral + bad
+
+  // Jos palautetta ei ole annettu, näytetään viesti "No feedback given"
+  if (all === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
+  const average = all === 0 ? 0 : (good - bad) / all
+  const positive = all === 0 ? 0 : (good / all) * 100
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      {/* Taulukko, jossa näytetään tilastotiedot */}
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive + ' %'} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -8,7 +61,11 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
